fb025b2a14cab43c59c85b5e284e2337d1c3c2ea
Duong Thien Chi <thienchidtc@gmail.com>
2024-12-11 15:31:10 +0700

    hash pass

--- Components/auth/api/authController.js
+++ Components/auth/api/authController.js
@@ -1,29 +1,31 @@
-const User = require('../data-access/AdminModel');
-
+const User = require("../data-access/AdminModel");
+const bcrypt = require("bcrypt");
 const authController = {
-  loginUser: async (req, res) => {
-    try {
-      const { email, password } = req.body;
+    loginUser: async (req, res) => {
+        try {
+            const { email, password } = req.body;
 
+            const user = await User.findOne({ email });
 
-      const user = await User.findOne({ email });
+            const isValidPassword = await bcrypt.compare(
+                password,
+                user.password
+            );
+            if (user && isValidPassword) {
+                req.session.isLoggedIn = true;
+                req.session.userId = user._id;
 
-      if (user && user.password === password) {
+                return res.redirect("/home");
+            }
 
-        req.session.isLoggedIn = true;
-        req.session.userId = user._id; 
-
-        return res.redirect('/home');
-      }
-
-      req.flash('errorMessage', 'Invalid email or password!');
-      return res.redirect('/');
-    } catch (error) {
-      console.error('Error logging in:', error);
-      req.flash('errorMessage', 'Internal Server Error');
-      return res.redirect('/');
-    }
-  },
+            req.flash("errorMessage", "Invalid email or password!");
+            return res.redirect("/");
+        } catch (error) {
+            console.error("Error logging in:", error);
+            req.flash("errorMessage", "Internal Server Error");
+            return res.redirect("/");
+        }
+    },
 };
 
 module.exports = authController;
--- Components/profile/api/profileController.js
+++ Components/profile/api/profileController.js
@@ -1,58 +1,61 @@
-const Admin = require("../../auth/data-access/AdminModel"); 
-const bcrypt = require('bcrypt');
+const Admin = require("../../auth/data-access/AdminModel");
+const bcrypt = require("bcrypt");
 
 const ProfileController = {
-  GetProfilePage: async (req, res) => {
-    try {
-      const userId = req.session.userId;
-      const user = await Admin.findById(userId);
+    GetProfilePage: async (req, res) => {
+        try {
+            const userId = req.session.userId;
+            const user = await Admin.findById(userId);
 
-      if (!user) {
-        return res.status(404).send("User not found");
-      }
+            if (!user) {
+                return res.status(404).send("User not found");
+            }
 
-      res.render("pages/profiles", {
-        user,
-        title: "Profile Page",
-        showSidebar: true,
-        showTopbar: true,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
-  },
+            res.render("pages/profiles", {
+                user,
+                title: "Profile Page",
+                showSidebar: true,
+                showTopbar: true,
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        }
+    },
 
-  UpdateProfile: async (req, res) => {
-    try {
-      const userId = req.session.userId; 
-      console.log("Received data in backend:", req.body); 
-  
-      const { name, email, address, contact, password } = req.body;
-  
-      const updatedData = {};
-  
-      if (name) updatedData.name = name;
-      if (email) updatedData.email = email;
-      if (address) updatedData.address = address;
-      if (contact) updatedData.contact = contact;
-  
-      if (password) updatedData.password = password;
+    UpdateProfile: async (req, res) => {
+        try {
+            const userId = req.session.userId;
+            console.log("Received data in backend:", req.body);
 
-  
-      console.log("Updated Data:", updatedData); 
-  
-      const updatedUser = await Admin.findByIdAndUpdate(userId, updatedData, { new: true });
-  
-      if (!updatedUser) {
-        return res.status(404).json({ success: false, message: 'User not found' });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-  
+            const { name, email, address, contact, password } = req.body;
+
+            const updatedData = {};
+
+            if (name) updatedData.name = name;
+            if (email) updatedData.email = email;
+            if (address) updatedData.address = address;
+            if (contact) updatedData.contact = contact;
+            const hashedPassword = await bcrypt.hash(password, 10);
+            if (password) updatedData.password = hashedPassword;
+
+            console.log("Updated Data:", updatedData);
+
+            const updatedUser = await Admin.findByIdAndUpdate(
+                userId,
+                updatedData,
+                { new: true }
+            );
+
+            if (!updatedUser) {
+                return res
+                    .status(404)
+                    .json({ success: false, message: "User not found" });
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    },
 };
 
 module.exports = ProfileController;
