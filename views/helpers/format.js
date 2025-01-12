38467fe9798e0950d81656a0d1a1c6f2b5e94c88
Sciderus <huydska11@gmail.com>
2025-01-12 19:18:23 +0700

    fix display multi-image

--- views/helpers/format.js
+++ views/helpers/format.js
@@ -9,3 +9,7 @@ hbs.registerHelper("formatDate", (date) => {
     const gmtIndex = dateString.indexOf("GMT");
     return dateString.substring(0, gmtIndex);
 });
+
+hbs.registerHelper("getThumbnail", function (imgs) {
+    return imgs[0];
+});
