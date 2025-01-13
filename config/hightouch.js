408b5d9784bf14420da71f11a42513b72473c5cd
Sciderus <huydska11@gmail.com>
2025-01-13 20:57:25 +0700

    fix api key in high touch

--- config/hightouch.js
+++ config/hightouch.js
@@ -1,27 +1,25 @@
-const axios = require('axios');
+const axios = require("axios");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 
 const url = process.env.HIGHTOUCH_URL;
 const api_key = process.env.HIGHTOUCH_KEY;
 
-async function syncDataByHighTouch(){
+async function syncDataByHighTouch() {
     try {
         const response = await axios.post(
             url,
             {}, // Add payload if required
             {
                 headers: {
-                    Authorization: 'Bearer' + api_key,
-                    'Content-Type': 'application/json',
+                    Authorization: "Bearer " + api_key,
+                    "Content-Type": "application/json",
                 },
             }
         );
-        console.log('Sync triggered successfully:', response.data);
-        return response.data; // Return response for use elsewhere    
+        return response.data;
     } catch (error) {
-        console.error('Error triggering sync:', error.response?.data || error.message);
-        throw error; // Rethrow the error for further handling
+        throw error;
     }
 }
 
